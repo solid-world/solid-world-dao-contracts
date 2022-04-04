8e12fe2888d9cb86fbfec6d3985e9c0178fb626c
Flavio Gouvea <flaviogouveaneto@gmail.com>
2022-04-04 14:44:58 -0300

    update sct treasury interface

--- contracts/interfaces/ISCTCarbonTreasury.sol
+++ contracts/interfaces/ISCTCarbonTreasury.sol
@@ -21,15 +21,22 @@ interface ISCTCarbonTreasury {
         bool isRedeemed;
     }
 
+    struct SaleApproval {
+        uint256 amount;
+        uint256 totalValue;
+    }
+
     event Deposited(address indexed token, uint256 indexed tokenId, address indexed owner, uint256 amount);
-    event Sold(address indexed token, uint256 indexed tokenId, address indexed owner, address buyer, uint256 amount);
+    event ApprovedSell(address indexed token, uint256 indexed tokenId, address owner, uint256 amount, uint256 totalValue);
+    event Sold(address indexed token, uint256 indexed tokenId, address indexed owner, address buyer, uint256 amount, uint256 totalValue);
     event UpdatedInfo(address indexed token, uint256 indexed tokenId, bool isActive);
     event Permissioned(STATUS indexed status, address token, bool result);
     event PermissionOrdered(STATUS indexed status, address token);
 
     function initialize() external;
-    function deposit(address _token, uint256 _tokenId, uint256 _amount,address _owner) external returns(bool);
-    function sell(address _token, uint256 _tokenId, uint256 _amount, uint256 _totalValue, address _buyer) external returns(bool);
+    function depositReserveToken(address _token, uint256 _tokenId, uint256 _amount,address _owner) external returns(bool);
+    function approveReserveTokenSell(address _token, uint256 _tokenId, uint256 _amount, uint256 _totalValue) external returns(bool);
+    function buyReserveToken(address _token, uint256 _tokenId, address _owner, uint256 _amount, uint256 _totalValue) external returns(bool);
     function createOrUpdateCarbonProject(CarbonProject memory _carbonProject) external returns(bool)
     function enable(STATUS _status, address _address) external returns(bool);
     function disable(STATUS _status, address _address) external returns(bool);
