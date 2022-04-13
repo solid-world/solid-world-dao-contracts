647aafeae7e1e593332fc1f6ed85168dc8b369d1
Emil Ibatullin <emil.ibatullin@gmail.com>
2022-04-13 16:31:55 -0300

    Add fancy output

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,4 +1,5 @@
 const { task } = require('hardhat/config');
+const pico = require('picocolors');
 const { getAccounts, getDeployer} = require('./accounts');
 
 task('deploy', 'Deploys DAO Management, SCT and Treasury contracts')
@@ -8,6 +9,13 @@ task('deploy', 'Deploys DAO Management, SCT and Treasury contracts')
     const deployerWallet = await getDeployer(ethers);
     const { governor, guardian, policy, vault } = await getAccounts(deployerWallet.address);
 
+    console.log(pico.dim('Governor: '.padStart(10) + pico.green(governor)));
+    console.log(pico.dim('Guardian: '.padStart(10) + pico.green(guardian)));
+    console.log(pico.dim('Policy: '.padStart(10) + pico.green(policy)));
+    console.log(pico.dim('Vault: '.padStart(10) + pico.green(vault)));
+
+    console.log('\n');
+
     const SolidDaoManagement = await ethers.getContractFactory('SolidDaoManagement', deployerWallet);
     const solidDaoManagement = await SolidDaoManagement.deploy(
       governor,
@@ -16,11 +24,11 @@ task('deploy', 'Deploys DAO Management, SCT and Treasury contracts')
       vault,
     );
     await solidDaoManagement.deployed()
-    console.log('DAO Management Address:'.padStart(25), solidDaoManagement.address);
+    console.log('DAO Management Address: '.padStart(24), pico.green(solidDaoManagement.address));
 
     const SctToken = await ethers.getContractFactory('SCTERC20Token', deployerWallet);
     const sctToken = await SctToken.deploy(solidDaoManagement.address);
-    console.log('SCT Token Address:'.padStart(25), sctToken.address);
+    console.log('SCT Token Address: '.padStart(24), pico.green(sctToken.address));
 
     const Treasury = await ethers.getContractFactory('SCTCarbonTreasury', deployerWallet);
     const treasury = await Treasury.deploy(
@@ -28,5 +36,5 @@ task('deploy', 'Deploys DAO Management, SCT and Treasury contracts')
       sctToken.address,
       0
     );
-    console.log('Treasury Address:'.padStart(25), treasury.address);
+    console.log('Treasury Address: '.padStart(24), pico.green(treasury.address));
   });
