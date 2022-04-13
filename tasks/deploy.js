6965aa1e361a0ed2c56c536a39c7d1fc7120994a
Emil Ibatullin <emil.ibatullin@gmail.com>
2022-04-13 15:43:48 -0300

    Pass deployer's wallet

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -5,10 +5,10 @@ task('deploy', 'Deploys DAO Management, SCT and Treasury contracts')
   .setAction(async (taskArgs, hre) => {
     const { ethers } = hre;
 
-    const deployerWallet = await getDeployer(hre.ethers);
+    const deployerWallet = await getDeployer(ethers);
     const { governor, guardian, policy, vault } = await getAccounts(deployerWallet.address);
 
-    const SolidDaoManagement = await ethers.getContractFactory('SolidDaoManagement');
+    const SolidDaoManagement = await ethers.getContractFactory('SolidDaoManagement', deployerWallet);
     const solidDaoManagement = await SolidDaoManagement.deploy(
       governor,
       guardian,
@@ -18,11 +18,11 @@ task('deploy', 'Deploys DAO Management, SCT and Treasury contracts')
     await solidDaoManagement.deployed()
     console.log('DAO Management Address:'.padStart(25), solidDaoManagement.address);
 
-    const SctToken = await ethers.getContractFactory('SCTERC20Token');
+    const SctToken = await ethers.getContractFactory('SCTERC20Token', deployerWallet);
     const sctToken = await SctToken.deploy(solidDaoManagement.address);
     console.log('SCT Token Address:'.padStart(25), sctToken.address);
 
-    const Treasury = await ethers.getContractFactory('SCTCarbonTreasury');
+    const Treasury = await ethers.getContractFactory('SCTCarbonTreasury', deployerWallet);
     const treasury = await Treasury.deploy(
       solidDaoManagement.address,
       sctToken.address,
