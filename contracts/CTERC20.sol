c791852b8b4ac004ebb5c32c00e341d2dc3db56c
Jeff Prestes <jeffprestes@gmail.com>
2022-06-09 10:49:00 -0300

    39-CTTemplate (#46)
    * recieve params & remove deps
    
    * adjust constructor & functions to ERC20 pattern
    
    * update CTERC20TokenTemplate abi and bytecodes
    
    Co-authored-by: Flavio Gouvea <flaviogouveaneto@gmail.com>

---
+++ contracts/CTERC20.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.13;
+
+import "./interfaces/IERC20.sol";
+import "./interfaces/IERC20Permit.sol";
+import "./lib/ERC20Permit.sol";
+
+/**
+ * @title CT ERC-20 Token Template
+ * @author Solid World DAO
+ * @notice CT Token ERC-20 Template
+ */
+contract CTERC20TokenTemplate is ERC20Permit {
+
+    // Treasury that manages this Token
+    address public treasury;
+
+    // Contract deployer
+    address public deployer;
+
+    // Is the Treasury initalized
+    bool public isInitialized;
+
+    // @notice: This modifier allows only the Treasury of the token perform the operation
+    modifier onlyTreasury() {
+        require(isInitialized, "Token needs to be initialized");
+        require(msg.sender == treasury, "Only Treasury can perform this operation");
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol, 18) ERC20Permit(_name) {
+        deployer = msg.sender;
+    }    
+
+
+    // @notice: After Token and Tresury have been deployed the Deployer need to initialize the Token informing 
+    // which Treasury is going to manage him.
+    function initialize(address _treasury) external {
+        require(deployer == msg.sender, "Only Deployer must initialize the token");
+        require(!isInitialized, "Token already has been initialized");
+        treasury = _treasury;
+        isInitialized = true;
+    }
+
+    function mint(address account_, uint256 amount_) external onlyTreasury {
+        _mint(account_, amount_);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account_, uint256 amount_) external {
+        _burnFrom(account_, amount_);
+    }
+
+    function _burnFrom(address account_, uint256 amount_) internal {
+        uint256 senderAllowance = allowance(account_, msg.sender);
+        require(senderAllowance - amount_ >= 0, "ERC20: burn amount exceeds allowance");
+        senderAllowance -= amount_;
+        _approve(account_, msg.sender, senderAllowance);
+        _burn(account_, amount_);
+    }
+}
--- router.md
+++ router.md
@@ -2,20 +2,20 @@
 
 ## SCT Smart Contracts
 
-### 1. Deploy SCT SolidDaoManagement
+### 1. Deploy CT SolidDaoManagement
 
 1.1. Deploy SolidDaoManagement and set `governor`, `guardian`, `policy` and `vault` addresses in constructor.
 
-### 2. Deploy SCTERC20 Token
+### 2. Deploy CTERC20 Token
 
-2.1. Deploy SCTERC20 and set SCT SolidDaoManagement `address` as `authority` in constructor.
+2.1. Deploy CTERC20TokenTemplate and set token `_name` and token `_symbol` in constructor.
 
-### 3. Deploy and initialize SCT Carbon Treasury
+### 3. Deploy and initialize CT Treasury
 
-3.1. Deploy SCTCarbonTreasury and set SCTERC20 `address` as `_sct`, timelock `uint256` (blocks need to enable one order) as `_timelock` and SCT SolidDaoManagement `address` as `_authority` in constructor.
+3.1. Deploy CTTreasury and set CTERC20 `address` as `_cterc20`, timelock `uint256` (blocks need to enable one order) as `_timelock` and CT SolidDaoManagement `address` as `_authority` in constructor.
 
 3.2. Initialize calling `initialize()` function.
 
-### 4. Add SCT Carbon Treasury as SCT vault in SCT SolidDaoManagement
+### 4. Initialize CTERC20 and Add CT Carbon Treasury
 
-4.1. Call SCT SolidDaoManagement smart contract `pushVault()` function passing SCT Carbon Treasury `address` as `_newVault` and `true` as `_effectiveImmediately` as parameters.
+4.1. Call CTERC20TokenTemplate smart contract `initialize()` function and set CT Treasury `address` as `_treasury_`.
