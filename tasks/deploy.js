3f05ab859981af79bce2794155bc76a22bfb4b21
Emil Ibatullin <emil.ibatullin@gmail.com>
2022-07-05 22:21:24 -0300

    Add predefined treasuries deployment

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -9,6 +9,11 @@ task('deploy', 'Deploys DAO Management, SCT and Treasury contracts')
     const { ethers } = hre;
 
     const deployerWallet = await getDeployer(ethers);
+
+    /**
+     * DAO Management deployment
+     */
+
     const { governor, guardian, policy, vault } = await getAccounts(deployerWallet.address);
 
     console.log(pico.dim('Governor: '.padStart(10) + pico.green(governor)));
@@ -28,18 +33,39 @@ task('deploy', 'Deploys DAO Management, SCT and Treasury contracts')
     await solidDaoManagement.deployed()
     console.log('DAO Management Address: '.padStart(24), pico.green(solidDaoManagement.address));
 
-    const CTToken = await ethers.getContractFactory('CTERC20TokenTemplate', deployerWallet);
-    const ctToken = await CTToken.deploy("CTTest", "CTTest");
-    console.log('CT Token Address: '.padStart(24), pico.green(ctToken.address));
+    /**
+     * Treasuries and ERC20 tokens deployment
+     */
 
+    /** @type {[string, string][]} Array of tuples - [treasury's category, ERC20 token's symbol] */
+    const treasuries = [
+      ['ForestConservation', 'CTFC'],
+      ['Livestock', 'CTL'],
+      ['WasteManagement', 'CTWM'],
+      ['Agriculture', 'CTA'],
+      ['EnergyProduction', 'CTEP'],
+    ];
+
+    const CTToken = await ethers.getContractFactory('CTERC20TokenTemplate', deployerWallet);
     const Treasury = await ethers.getContractFactory('CTTreasury', deployerWallet);
-    const treasury = await Treasury.deploy(
-      solidDaoManagement.address,
-      ctToken.address,
-      0,
-      "REED+",
-      "0x8B3A08b22d25C60e4b2BfD984e331568ECa4C299",
-      2
-    );
-    console.log('Treasury Address: '.padStart(24), pico.green(treasury.address));
+
+    for await (const [treasuryName, tokenSymbol] of treasuries) {
+      console.log('Deploying %s treasury...', pico.green(treasuryName));
+
+      const ctToken = await CTToken.deploy(tokenSymbol, tokenSymbol);
+
+      const treasury = await Treasury.deploy(
+        solidDaoManagement.address,
+        ctToken.address,
+        0,
+        treasuryName,
+        "0x8B3A08b22d25C60e4b2BfD984e331568ECa4C299",
+        2
+      );
+
+      console.log('Treasury Address: '.padStart(24), pico.green(treasury.address));
+      console.log('CT Token Address: '.padStart(24), pico.green(ctToken.address));
+
+      await ctToken.initialize(treasury.address)
+    }
   });
