ec341b6b27eecad62baef7d7dafa35174c84d2f5
Emil Ibatullin <emil.ibatullin@gmail.com>
2022-07-08 12:44:53 -0300

    Refactor depositing

--- tasks/ct-treasury-seed/deposit-seed.js
+++ tasks/ct-treasury-seed/deposit-seed.js
@@ -63,72 +63,38 @@ task('deposit-seed', 'Deposit ERC1155 in CT Treasuries')
 
     console.log('Starting deposits...');
 
-    const treasuryForestConservationContract = new ethers.Contract(treasuryAddresses[0], ctTreasuryAbi, ownerWallet);
-    const treasuryLivestockContract = new ethers.Contract(treasuryAddresses[1], ctTreasuryAbi, ownerWallet);
-    const treasuryWasteManagementContract = new ethers.Contract(treasuryAddresses[2], ctTreasuryAbi, ownerWallet);
-    const treasuryAgricultureContract = new ethers.Contract(treasuryAddresses[3], ctTreasuryAbi, ownerWallet);
-    const treasuryEnergyContract = new ethers.Contract(treasuryAddresses[4], ctTreasuryAbi, ownerWallet);
+    const deposits = [
+      { projectId: 1,  tokenAmount: 5000,  treasuryAddress: treasuryAddresses[0] },
+      { projectId: 2,  tokenAmount: 6000,  treasuryAddress: treasuryAddresses[0] },
+      { projectId: 3,  tokenAmount: 7000,  treasuryAddress: treasuryAddresses[0] },
+      { projectId: 4,  tokenAmount: 15000, treasuryAddress: treasuryAddresses[3] },
+      { projectId: 5,  tokenAmount: 9000,  treasuryAddress: treasuryAddresses[1] },
+      { projectId: 6,  tokenAmount: 10000, treasuryAddress: treasuryAddresses[3] },
+      { projectId: 7,  tokenAmount: 10000, treasuryAddress: treasuryAddresses[0] },
+      { projectId: 8,  tokenAmount: 11000, treasuryAddress: treasuryAddresses[3] },
+      { projectId: 9,  tokenAmount: 7000,  treasuryAddress: treasuryAddresses[3] },
+      { projectId: 10, tokenAmount: 20000, treasuryAddress: treasuryAddresses[0] },
+      { projectId: 11, tokenAmount: 13000, treasuryAddress: treasuryAddresses[0] },
+      { projectId: 12, tokenAmount: 18000, treasuryAddress: treasuryAddresses[0] },
+      { projectId: 13, tokenAmount: 25000, treasuryAddress: treasuryAddresses[2] },
+      { projectId: 14, tokenAmount: 14000, treasuryAddress: treasuryAddresses[4] },
+      { projectId: 15, tokenAmount: 10000, treasuryAddress: treasuryAddresses[0] },
+    ];
 
-    const deposit1 = await treasuryForestConservationContract.depositReserveToken(carbonProjectTokenAddress, 1, 5000, ownerWallet.address);
-    await deposit1.wait();
-    console.log('Deposit Project 1 tx: '.padStart(24), pico.green(deposit1.hash));
+    for (const { projectId, tokenAmount, treasuryAddress } of deposits) {
+      assert(treasuryAddress != null, 'Treasury address is undefined.')
+      const treasuryContract = new ethers.Contract(treasuryAddress, ctTreasuryAbi, ownerWallet);
 
-    const deposit2 = await treasuryForestConservationContract.depositReserveToken(carbonProjectTokenAddress, 2, 6000, ownerWallet.address);
-    await deposit2.wait();
-    console.log('Deposit Project 2 tx: '.padStart(24), pico.green(deposit2.hash));
+      const depositTx = await treasuryContract.depositReserveToken(
+        carbonProjectTokenAddress,
+        projectId,
+        tokenAmount,
+        ownerWallet.address
+      );
+      await depositTx.wait();
 
-    const deposit3 = await treasuryForestConservationContract.depositReserveToken(carbonProjectTokenAddress, 3, 7000, ownerWallet.address);
-    await deposit3.wait();
-    console.log('Deposit Project 3 tx: '.padStart(24), pico.green(deposit3.hash));
-
-    const deposit4 = await treasuryAgricultureContract.depositReserveToken(carbonProjectTokenAddress, 4, 15000, ownerWallet.address);
-    await deposit4.wait();
-    console.log('Deposit Project 4 tx: '.padStart(24), pico.green(deposit4.hash));
-
-    const deposit5 = await treasuryLivestockContract.depositReserveToken(carbonProjectTokenAddress, 5, 9000, ownerWallet.address);
-    await deposit5.wait();
-    console.log('Deposit Project 5 tx: '.padStart(24), pico.green(deposit5.hash));
-
-    const deposit6 = await treasuryAgricultureContract.depositReserveToken(carbonProjectTokenAddress, 6, 10000, ownerWallet.address);
-    await deposit6.wait();
-    console.log('Deposit Project 6 tx: '.padStart(24), pico.green(deposit6.hash));
-
-    const deposit7 = await treasuryForestConservationContract.depositReserveToken(carbonProjectTokenAddress, 7, 10000, ownerWallet.address);
-    await deposit7.wait();
-    console.log('Deposit Project 7 tx: '.padStart(24), pico.green(deposit7.hash));
-
-    const deposit8 = await treasuryAgricultureContract.depositReserveToken(carbonProjectTokenAddress, 8, 11000, ownerWallet.address);
-    await deposit8.wait();
-    console.log('Deposit Project 8 tx: '.padStart(24), pico.green(deposit8.hash));
-
-    const deposit9 = await treasuryAgricultureContract.depositReserveToken(carbonProjectTokenAddress, 9, 7000, ownerWallet.address);
-    await deposit9.wait();
-    console.log('Deposit Project 9 tx: '.padStart(24), pico.green(deposit9.hash));
-
-    const deposit10 = await treasuryForestConservationContract.depositReserveToken(carbonProjectTokenAddress, 10, 20000, ownerWallet.address);
-    await deposit10.wait();
-    console.log('Deposit Project 10 tx: '.padStart(24), pico.green(deposit10.hash));
-
-    const deposit11 = await treasuryForestConservationContract.depositReserveToken(carbonProjectTokenAddress, 11, 13000, ownerWallet.address);
-    await deposit11.wait();
-    console.log('Deposit Project 11 tx: '.padStart(24), pico.green(deposit11.hash));
-
-    const deposit12 = await treasuryForestConservationContract.depositReserveToken(carbonProjectTokenAddress, 12, 18000, ownerWallet.address);
-    await deposit12.wait();
-    console.log('Deposit Project 12 tx: '.padStart(24), pico.green(deposit12.hash));
-
-    const deposit13 = await treasuryWasteManagementContract.depositReserveToken(carbonProjectTokenAddress, 13, 25000, ownerWallet.address);
-    await deposit13.wait();
-    console.log('Deposit Project 13 tx: '.padStart(24), pico.green(deposit13.hash));
-
-    const deposit14 = await treasuryEnergyContract.depositReserveToken(carbonProjectTokenAddress, 14, 14000, ownerWallet.address);
-    await deposit14.wait();
-    console.log('Deposit Project 14 tx: '.padStart(24), pico.green(deposit14.hash));
-
-    const deposit15 = await treasuryForestConservationContract.depositReserveToken(carbonProjectTokenAddress, 15, 10000, ownerWallet.address);
-    await deposit15.wait();
-    console.log('Deposit Project 15 tx: '.padStart(24), pico.green(deposit15.hash));
+      console.log('Deposit Project %s tx: %s', pico.green(projectId), pico.green(depositTx.hash));
+    }
 
     console.log('All deposit-seed tasks finished with success');
-
   });
