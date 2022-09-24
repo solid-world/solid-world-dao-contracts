bfc9e73eccc32938eacfbfcff2b0e1f83248b17f
Emil Ibatullin <emil.ibatullin@gmail.com>
2022-09-24 10:45:04 +0300

    Add addProject method

--- contracts/SolidWorldManager.sol
+++ contracts/SolidWorldManager.sol
@@ -8,7 +8,15 @@ import "./Erc20Deployer.sol";
 contract SolidWorldManager is Initializable, OwnableUpgradeable {
     Erc20Deployer erc20Deployer;
 
+    mapping(uint256 => bool) public categoryIds;
+
+    mapping(uint256 => bool) public projectIds;
+
     mapping(uint256 => address) public categoryToken;
+
+    mapping(uint256 => uint256[]) internal categoryProjects;
+
+    mapping(uint256 => uint256) internal projectCategory;
 
     function initialize(Erc20Deployer _erc20Deployer) public initializer {
         erc20Deployer = _erc20Deployer;
@@ -22,7 +30,22 @@ contract SolidWorldManager is Initializable, OwnableUpgradeable {
         string memory tokenSymbol
     ) public {
         // todo: add sanity check, like categoryId value, overriding, etc
+        categoryIds[categoryId] = true;
+
         address tokenAddress = erc20Deployer.deploy(address(this), tokenName, tokenSymbol);
         categoryToken[categoryId] = tokenAddress;
+    }
+
+    // todo: add authorization
+    function addProject(uint256 categoryId, uint256 projectId) public {
+        require(categoryIds[categoryId]);
+        require(!projectIds[projectId]);
+
+        categoryProjects[categoryId].push(projectId);
+        projectIds[projectId] = true;
+    }
+
+    function getProjectIdsByCategory(uint256 categoryId) public view returns (uint256[] memory) {
+        return categoryProjects[categoryId];
     }
 }
--- test/SolidWorldManager.t.sol
+++ test/SolidWorldManager.t.sol
@@ -17,8 +17,43 @@ contract SolidWorldManagerTest is Test {
 
     function testAddCategory() public {
         assertEq(manager.categoryToken(1), address(0));
+        assertEq(manager.categoryIds(1), false);
+
         manager.addCategory(1, "Test token", "TT");
+
         assertNotEq(manager.categoryToken(1), address(0));
+        assertEq(manager.categoryIds(1), true);
+    }
+
+    function testAddProject() public {
+        assertEq(manager.projectIds(5), false);
+
+        manager.addCategory(3, "Test token", "TT");
+        manager.addProject(3, 5);
+
+        assertEq(manager.projectIds(5), true);
+    }
+
+    function testAddMultipleProjects() public {
+        assertEq(manager.getProjectIdsByCategory(3).length, 0);
+
+        manager.addCategory(3, "Test token", "TT");
+        manager.addProject(3, 5);
+        manager.addProject(3, 7);
+
+        assertEq(manager.getProjectIdsByCategory(3).length, 2);
+        assertEq(manager.getProjectIdsByCategory(3)[0], 5);
+        assertEq(manager.getProjectIdsByCategory(3)[1], 7);
+    }
+
+    function testFailAddProjectWhenCategoryDoesntExist() public {
+        manager.addProject(3, 5);
+    }
+
+    function testFailAddProjectWhenProjectAlreadyAdded() public {
+        manager.addCategory(3, "Test token", "TT");
+        manager.addProject(3, 5);
+        manager.addProject(3, 5);
     }
 
     function assertNotEq(address a, address b) private {
