774d08e6227bcc8c4c752c0e49b5c861811f09e6
Andrei Stefan <andrei.stefan.work@gmail.com>
2022-12-01 10:12:09 +0200

    Test EmissionManager (#167)

--- test/rewards/EmissionManager.t.sol
+++ test/rewards/EmissionManager.t.sol
@@ -8,6 +8,12 @@ import "../../contracts/SolidWorldManager.sol";
 import "../../contracts/rewards/RewardsController.sol";
 
 contract EmissionManagerTest is Test {
+    event EmissionAdminUpdated(
+        address indexed reward,
+        address indexed oldAdmin,
+        address indexed newAdmin
+    );
+
     EmissionManager emissionManager;
     address carbonRewardsManager;
     address controller;
@@ -32,6 +38,209 @@ contract EmissionManagerTest is Test {
         vm.label(controller, "controller");
         vm.label(owner, "owner");
         vm.label(rewardsVault, "rewardsVault");
+    }
+
+    function testRecurrentSetup() public {
+        assertEq(emissionManager.owner(), owner);
+        assertEq(emissionManager.getCarbonRewardsManager(), carbonRewardsManager);
+        assertEq(address(emissionManager.getRewardsController()), controller);
+
+        vm.expectRevert(abi.encodeWithSelector(PostConstruct.AlreadyInitialized.selector));
+        emissionManager.setup(
+            IWeeklyCarbonRewardsManager(carbonRewardsManager),
+            IRewardsController(controller),
+            owner
+        );
+    }
+
+    function testConfigureAssets() public {
+        RewardsDataTypes.RewardsConfigInput[]
+            memory config = new RewardsDataTypes.RewardsConfigInput[](2);
+        config[0].reward = vm.addr(113);
+        config[0].asset = vm.addr(114);
+        config[0].rewardOracle = IEACAggregatorProxy(vm.addr(115));
+        config[0].emissionPerSecond = 100;
+        config[0].distributionEnd = 1000;
+        config[0].totalStaked = 10000;
+
+        config[1].reward = vm.addr(116);
+        config[1].asset = vm.addr(117);
+        config[1].rewardOracle = IEACAggregatorProxy(vm.addr(118));
+        config[1].emissionPerSecond = 200;
+        config[1].distributionEnd = 2000;
+        config[1].totalStaked = 20000;
+
+        address emissionAdmin = vm.addr(119);
+        vm.startPrank(owner);
+        emissionManager.setEmissionAdmin(config[0].reward, emissionAdmin);
+        emissionManager.setEmissionAdmin(config[1].reward, emissionAdmin);
+        vm.stopPrank();
+
+        vm.mockCall(
+            controller,
+            abi.encodeWithSelector(IRewardsController.configureAssets.selector),
+            abi.encode()
+        );
+        vm.expectCall(controller, abi.encodeCall(IRewardsController.configureAssets, config));
+        vm.prank(emissionAdmin);
+        emissionManager.configureAssets(config);
+    }
+
+    function testConfigureAssets_failsIfNotCalledByEmissionAdmin() public {
+        RewardsDataTypes.RewardsConfigInput[]
+            memory config = new RewardsDataTypes.RewardsConfigInput[](2);
+        config[0].reward = vm.addr(113);
+        config[0].asset = vm.addr(114);
+        config[0].rewardOracle = IEACAggregatorProxy(vm.addr(115));
+        config[0].emissionPerSecond = 100;
+        config[0].distributionEnd = 1000;
+        config[0].totalStaked = 10000;
+
+        config[1].reward = vm.addr(116);
+        config[1].asset = vm.addr(117);
+        config[1].rewardOracle = IEACAggregatorProxy(vm.addr(118));
+        config[1].emissionPerSecond = 200;
+        config[1].distributionEnd = 2000;
+        config[1].totalStaked = 20000;
+
+        address emissionAdmin = vm.addr(119);
+        vm.prank(owner);
+        emissionManager.setEmissionAdmin(config[0].reward, emissionAdmin);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IEmissionManager.NotEmissionAdmin.selector,
+                emissionAdmin,
+                config[1].reward
+            )
+        );
+        vm.prank(emissionAdmin);
+        emissionManager.configureAssets(config);
+    }
+
+    function testSetRewardOracle_failsIfNotCalledByEmissionAdmin() public {
+        address notAnAdmin = vm.addr(119);
+        address reward = vm.addr(113);
+        IEACAggregatorProxy rewardOracle = IEACAggregatorProxy(vm.addr(115));
+
+        vm.expectRevert(
+            abi.encodeWithSelector(IEmissionManager.NotEmissionAdmin.selector, notAnAdmin, reward)
+        );
+        vm.prank(notAnAdmin);
+        emissionManager.setRewardOracle(reward, rewardOracle);
+    }
+
+    function testSetRewardOracle() public {
+        address emissionAdmin = vm.addr(119);
+        address reward = vm.addr(113);
+        IEACAggregatorProxy rewardOracle = IEACAggregatorProxy(vm.addr(115));
+
+        vm.prank(owner);
+        emissionManager.setEmissionAdmin(reward, emissionAdmin);
+
+        vm.mockCall(
+            controller,
+            abi.encodeWithSelector(IRewardsController.setRewardOracle.selector),
+            abi.encode()
+        );
+        vm.expectCall(
+            controller,
+            abi.encodeCall(IRewardsController.setRewardOracle, (reward, rewardOracle))
+        );
+        vm.prank(emissionAdmin);
+        emissionManager.setRewardOracle(reward, rewardOracle);
+    }
+
+    function testSetDistributionEnd_failsIfNotCalledByEmissionAdmin() public {
+        address notAnAdmin = vm.addr(119);
+        address reward = vm.addr(113);
+        address asset = vm.addr(114);
+        uint32 distributionEnd = 1000;
+
+        vm.expectRevert(
+            abi.encodeWithSelector(IEmissionManager.NotEmissionAdmin.selector, notAnAdmin, reward)
+        );
+        vm.prank(notAnAdmin);
+        emissionManager.setDistributionEnd(asset, reward, distributionEnd);
+    }
+
+    function testSetDistributionEnd() public {
+        address emissionAdmin = vm.addr(119);
+        address reward = vm.addr(113);
+        address asset = vm.addr(114);
+        uint32 distributionEnd = 1000;
+
+        vm.prank(owner);
+        emissionManager.setEmissionAdmin(reward, emissionAdmin);
+
+        vm.mockCall(
+            controller,
+            abi.encodeWithSelector(IRewardsDistributor.setDistributionEnd.selector),
+            abi.encode()
+        );
+        vm.expectCall(
+            controller,
+            abi.encodeCall(IRewardsDistributor.setDistributionEnd, (asset, reward, distributionEnd))
+        );
+        vm.prank(emissionAdmin);
+        emissionManager.setDistributionEnd(asset, reward, distributionEnd);
+    }
+
+    function testSetEmissionPerSecond_failsIfNotCalledByEmissionAdmin() public {
+        address notAnAdmin = vm.addr(119);
+        address asset = vm.addr(114);
+        address[] memory rewards = new address[](2);
+        uint88[] memory emissionsPerSecond = new uint88[](2);
+
+        rewards[0] = vm.addr(113);
+        rewards[1] = vm.addr(116);
+        emissionsPerSecond[0] = 100;
+        emissionsPerSecond[1] = 200;
+
+        vm.prank(owner);
+        emissionManager.setEmissionAdmin(rewards[0], notAnAdmin);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IEmissionManager.NotEmissionAdmin.selector,
+                notAnAdmin,
+                rewards[1]
+            )
+        );
+        vm.prank(notAnAdmin);
+        emissionManager.setEmissionPerSecond(asset, rewards, emissionsPerSecond);
+    }
+
+    function testSetEmissionPerSecond() public {
+        address emissionAdmin = vm.addr(119);
+        address asset = vm.addr(114);
+        address[] memory rewards = new address[](2);
+        uint88[] memory emissionsPerSecond = new uint88[](2);
+
+        rewards[0] = vm.addr(113);
+        rewards[1] = vm.addr(116);
+        emissionsPerSecond[0] = 100;
+        emissionsPerSecond[1] = 200;
+
+        vm.startPrank(owner);
+        emissionManager.setEmissionAdmin(rewards[0], emissionAdmin);
+        emissionManager.setEmissionAdmin(rewards[1], emissionAdmin);
+        vm.stopPrank();
+
+        vm.mockCall(
+            controller,
+            abi.encodeWithSelector(IRewardsDistributor.setEmissionPerSecond.selector),
+            abi.encode()
+        );
+        vm.expectCall(
+            controller,
+            abi.encodeCall(
+                IRewardsDistributor.setEmissionPerSecond,
+                (asset, rewards, emissionsPerSecond)
+            )
+        );
+        vm.prank(emissionAdmin);
+        emissionManager.setEmissionPerSecond(asset, rewards, emissionsPerSecond);
     }
 
     function testUpdateCarbonRewardDistribution() public {
@@ -70,5 +279,94 @@ contract EmissionManagerTest is Test {
             )
         );
         emissionManager.updateCarbonRewardDistribution(assets, categoryIds);
+    }
+
+    function testSetClaimer_failsIfNotCalledByOwner() public {
+        address notOwner = vm.addr(119);
+        address claimer = vm.addr(115);
+        address user = vm.addr(116);
+
+        vm.expectRevert(abi.encodePacked("Ownable: caller is not the owner"));
+        vm.prank(notOwner);
+        emissionManager.setClaimer(user, claimer);
+    }
+
+    function testSetClaimer() public {
+        address claimer = vm.addr(115);
+        address user = vm.addr(116);
+
+        vm.mockCall(
+            controller,
+            abi.encodeWithSelector(IRewardsController.setClaimer.selector),
+            abi.encode()
+        );
+        vm.expectCall(controller, abi.encodeCall(IRewardsController.setClaimer, (user, claimer)));
+        vm.prank(owner);
+        emissionManager.setClaimer(user, claimer);
+    }
+
+    function testSetEmissionManager_failsIfNotCalledByOwner() public {
+        address notOwner = vm.addr(119);
+        address newEmissionManager = vm.addr(115);
+
+        vm.expectRevert(abi.encodePacked("Ownable: caller is not the owner"));
+        vm.prank(notOwner);
+        emissionManager.setEmissionManager(newEmissionManager);
+    }
+
+    function testSetEmissionManager() public {
+        address newEmissionManager = vm.addr(115);
+
+        vm.mockCall(
+            controller,
+            abi.encodeWithSelector(IRewardsDistributor.setEmissionManager.selector),
+            abi.encode()
+        );
+        vm.expectCall(
+            controller,
+            abi.encodeCall(IRewardsDistributor.setEmissionManager, newEmissionManager)
+        );
+        vm.prank(owner);
+        emissionManager.setEmissionManager(newEmissionManager);
+    }
+
+    function testSetEmissionAdmin_failsIfNotCalledByOwner() public {
+        address notOwner = vm.addr(119);
+        address emissionAdmin = vm.addr(115);
+        address reward = vm.addr(116);
+
+        vm.expectRevert(abi.encodePacked("Ownable: caller is not the owner"));
+        vm.prank(notOwner);
+        emissionManager.setEmissionAdmin(reward, emissionAdmin);
+    }
+
+    function testSetEmissionAdmin() public {
+        address emissionAdmin = vm.addr(115);
+        address reward = vm.addr(116);
+
+        vm.expectEmit(true, true, true, false, address(emissionManager));
+        emit EmissionAdminUpdated(reward, address(0), emissionAdmin);
+        vm.prank(owner);
+        emissionManager.setEmissionAdmin(reward, emissionAdmin);
+
+        assertEq(emissionManager.getEmissionAdmin(reward), emissionAdmin);
+    }
+
+    function testSetRewardsController_failsIfNotCalledByOwner() public {
+        address notOwner = vm.addr(119);
+        address newController = vm.addr(115);
+
+        vm.expectRevert(abi.encodePacked("Ownable: caller is not the owner"));
+        vm.prank(notOwner);
+        emissionManager.setRewardsController(newController);
+    }
+
+    function testSetRewardsController() public {
+        address newController = vm.addr(115);
+
+        vm.prank(owner);
+        emissionManager.setRewardsController(newController);
+
+        assertEq(address(emissionManager.getRewardsController()), newController);
     }
 }
