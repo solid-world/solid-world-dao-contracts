4a32c9cafa700aac2cbb5d42f82bb25ce3613a8b
Andrei Stefan <andrei.stefan.work@gmail.com>
2023-01-04 12:52:57 +0200

    pull up decollateralization fn to interface

--- contracts/SolidWorldManager.sol
+++ contracts/SolidWorldManager.sol
@@ -19,6 +19,7 @@ import "./CollateralizedBasketTokenDeployer.sol";
 import "./SolidWorldManagerStorage.sol";
 import "./interfaces/manager/ICarbonDomainRepository.sol";
 import "./interfaces/manager/ICollateralizationManager.sol";
+import "./interfaces/manager/IDecollateralizationManager.sol";
 
 contract SolidWorldManager is
     Initializable,
@@ -27,6 +28,7 @@ contract SolidWorldManager is
     ReentrancyGuardUpgradeable,
     IWeeklyCarbonRewardsManager,
     ICollateralizationManager,
+    IDecollateralizationManager,
     ICarbonDomainRepository,
     ISolidWorldManagerErrors,
     SolidWorldManagerStorage
@@ -168,13 +170,7 @@ contract SolidWorldManager is
         _storage.collateralizeBatch(batchId, amountIn, amountOutMin);
     }
 
-    /// @dev Decollateralizes `amountIn` of ERC20 tokens and sends `amountOut` ERC1155 tokens with id `batchId` to msg.sender
-    /// @dev prior to calling, msg.sender must approve SolidWorldManager to spend `amountIn` ERC20 tokens
-    /// @dev nonReentrant (_decollateralizeTokens), to avoid possible reentrancy after calling safeTransferFrom
-    /// @dev will trigger a rebalance of the Category
-    /// @param batchId id of the batch
-    /// @param amountIn ERC20 tokens to decollateralize
-    /// @param amountOutMin minimum output amount of ERC1155 tokens for transaction to succeed
+    /// @inheritdoc IDecollateralizationManager
     function decollateralizeTokens(
         uint batchId,
         uint amountIn,
@@ -185,14 +181,7 @@ contract SolidWorldManager is
         _rebalanceCategory(_storage.batchCategory[batchId]);
     }
 
-    /// @dev Bulk-decollateralizes ERC20 tokens into multiple ERC1155 tokens with specified amounts
-    /// @dev prior to calling, msg.sender must approve SolidWorldManager to spend `sum(amountsIn)` ERC20 tokens
-    /// @dev nonReentrant (_decollateralizeTokens), to avoid possible reentrancy after calling safeTransferFrom
-    /// @dev _batchIds must belong to the same Category
-    /// @dev will trigger a rebalance of the Category
-    /// @param batchIds ids of the batches
-    /// @param amountsIn ERC20 tokens to decollateralize
-    /// @param amountsOutMin minimum output amounts of ERC1155 tokens for transaction to succeed
+    /// @inheritdoc IDecollateralizationManager
     function bulkDecollateralizeTokens(
         uint[] calldata batchIds,
         uint[] calldata amountsIn,
@@ -232,12 +221,7 @@ contract SolidWorldManager is
         return _storage.simulateBatchCollateralization(batchId, amountIn);
     }
 
-    /// @dev Simulates decollateralization of `amountIn` ERC20 tokens for ERC1155 tokens with id `batchId`
-    /// @param batchId id of the batch
-    /// @param amountIn ERC20 tokens to decollateralize
-    /// @return amountOut ERC1155 tokens to be received by msg.sender
-    /// @return minAmountIn minimum amount of ERC20 tokens to decollateralize `amountOut` ERC1155 tokens with id `batchId`
-    /// @return minCbtDaoCut ERC20 tokens to be received by feeReceiver for decollateralizing minAmountIn ERC20 tokens
+    /// @inheritdoc IDecollateralizationManager
     function simulateDecollateralization(uint batchId, uint amountIn)
         external
         view
@@ -250,11 +234,7 @@ contract SolidWorldManager is
         (amountOut, minAmountIn, minCbtDaoCut) = _simulateDecollateralization(batchId, amountIn);
     }
 
-    /// @dev Computes relevant info for the decollateralization process involving batches
-    /// that match the specified `projectId` and `vintage`
-    /// @param projectId id of the project the batch belongs to
-    /// @param vintage vintage of the batch
-    /// @return result array of relevant info about matching batches
+    /// @inheritdoc IDecollateralizationManager
     function getBatchesDecollateralizationInfo(uint projectId, uint vintage)
         external
         view
@@ -308,6 +288,7 @@ contract SolidWorldManager is
     }
 
     // todo #121: add authorization
+    /// @inheritdoc IDecollateralizationManager
     function setDecollateralizationFee(uint16 decollateralizationFee) external {
         _setDecollateralizationFee(decollateralizationFee);
     }
