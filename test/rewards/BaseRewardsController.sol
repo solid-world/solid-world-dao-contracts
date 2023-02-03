32040b9938d0fc0b3dc4207d64ea2caefa183714
Andrei Stefan <andrei.stefan.work@gmail.com>
2023-02-03 17:48:41 +0200

    refactor RewardsController tests

---
+++ test/rewards/BaseRewardsController.sol
@@ -0,0 +1,237 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "../BaseTest.sol";
+import "../../contracts/interfaces/rewards/IRewardsController.sol";
+import "../../contracts/rewards/RewardsController.sol";
+
+abstract contract BaseRewardsControllerTest is BaseTest {
+    event AssetConfigUpdated(
+        address indexed asset,
+        address indexed reward,
+        uint oldEmission,
+        uint newEmission,
+        uint oldDistributionEnd,
+        uint newDistributionEnd,
+        uint assetIndex
+    );
+    event RewardOracleUpdated(address indexed reward, address indexed rewardOracle);
+    event ClaimerSet(address indexed user, address indexed claimer);
+    event RewardsVaultUpdated(address indexed rewardsVault);
+    event SolidStakingUpdated(address indexed solidStaking);
+
+    uint32 constant CURRENT_DATE = 1666016743;
+
+    IRewardsController rewardsController;
+    address solidStakingViewActions;
+    address rewardsVault;
+    address emissionManager;
+    address arbitraryAccount;
+
+    function setUp() public {
+        vm.warp(CURRENT_DATE);
+
+        solidStakingViewActions = vm.addr(1);
+        rewardsVault = vm.addr(2);
+        emissionManager = vm.addr(3);
+        arbitraryAccount = vm.addr(777);
+        rewardsController = new RewardsController();
+
+        vm.expectEmit(true, false, false, false, address(rewardsController));
+        emit SolidStakingUpdated(solidStakingViewActions);
+        vm.expectEmit(true, false, false, false, address(rewardsController));
+        emit RewardsVaultUpdated(rewardsVault);
+        RewardsController(address(rewardsController)).setup(
+            solidStakingViewActions,
+            rewardsVault,
+            emissionManager
+        );
+
+        _labelAccounts();
+    }
+
+    function _labelAccounts() private {
+        vm.label(address(rewardsController), "RewardsController");
+        vm.label(rewardsVault, "RewardsVault");
+        vm.label(emissionManager, "EmissionManager");
+        vm.label(solidStakingViewActions, "SolidStakingViewActions");
+        vm.label(arbitraryAccount, "ArbitraryAccount");
+    }
+
+    function _expectEmitAssetConfigUpdated(address asset, address reward) internal {
+        vm.expectEmit(true, true, false, false, address(rewardsController));
+        emit AssetConfigUpdated(asset, reward, 0, 0, 0, 0, 0);
+    }
+
+    function _expectEmitRewardOracleUpdated(address reward, IEACAggregatorProxy rewardOracle)
+        internal
+    {
+        vm.expectEmit(true, true, false, false, address(rewardsController));
+        emit RewardOracleUpdated(reward, address(rewardOracle));
+    }
+
+    function _expectEmit_ClaimerSet(address user, address claimer) internal {
+        vm.expectEmit(true, true, false, false, address(rewardsController));
+        emit ClaimerSet(user, claimer);
+    }
+
+    function _expectEmit_RewardsVaultUpdated(address _rewardsVault) internal {
+        vm.expectEmit(true, false, false, false, address(rewardsController));
+        emit RewardsVaultUpdated(_rewardsVault);
+    }
+
+    function _expectEmit_SolidStakingUpdated(address _solidStaking) internal {
+        vm.expectEmit(true, false, false, false, address(rewardsController));
+        emit SolidStakingUpdated(_solidStaking);
+    }
+
+    function _expectRevert_InvalidRewardOracle(address reward, IEACAggregatorProxy rewardOracle)
+        internal
+    {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IRewardsController.InvalidRewardOracle.selector,
+                reward,
+                address(rewardOracle)
+            )
+        );
+    }
+
+    function _expectRevert_NotEmissionManager(address sender) internal {
+        vm.expectRevert(
+            abi.encodeWithSelector(IRewardsDistributor.NotEmissionManager.selector, sender)
+        );
+    }
+
+    function _expectRevert_InvalidAssetDecimals(address asset) internal {
+        vm.expectRevert(
+            abi.encodeWithSelector(IRewardsDistributor.InvalidAssetDecimals.selector, asset)
+        );
+    }
+
+    function _expectRevert_NotSolidStaking(address sender) internal {
+        vm.expectRevert(
+            abi.encodeWithSelector(IRewardsController.NotSolidStaking.selector, sender)
+        );
+    }
+
+    function _expectRevert_InvalidInput() internal {
+        vm.expectRevert(abi.encodeWithSelector(IRewardsDistributor.InvalidInput.selector));
+    }
+
+    function _expectRevert_UnauthorizedClaimer(address claimer, address user) internal {
+        vm.expectRevert(
+            abi.encodeWithSelector(IRewardsController.UnauthorizedClaimer.selector, claimer, user)
+        );
+    }
+
+    function _makeTestDistributionConfig_invalidAssetDecimals()
+        internal
+        returns (RewardsDataTypes.DistributionConfig[] memory config)
+    {
+        config = _makeTestDistributionConfig();
+        _mockDistributionConfig_invalidAssetDecimals(config);
+    }
+
+    function _makeTestDistributionConfig_validAssetDecimals()
+        internal
+        returns (RewardsDataTypes.DistributionConfig[] memory config)
+    {
+        config = _makeTestDistributionConfig();
+        _mockDistributionConfig_validAssetDecimals(config);
+    }
+
+    function _makeTestDistributionConfig()
+        private
+        returns (RewardsDataTypes.DistributionConfig[] memory config)
+    {
+        config = new RewardsDataTypes.DistributionConfig[](2);
+        config[0].reward = vm.addr(4);
+        config[0].asset = vm.addr(5);
+        config[0].emissionPerSecond = 100;
+        config[0].distributionEnd = CURRENT_DATE;
+        config[0].rewardOracle = IEACAggregatorProxy(vm.addr(6));
+
+        config[1].reward = vm.addr(7);
+        config[1].asset = vm.addr(8);
+        config[1].emissionPerSecond = 200;
+        config[1].distributionEnd = CURRENT_DATE;
+        config[1].rewardOracle = IEACAggregatorProxy(vm.addr(9));
+    }
+
+    function _mockDistributionConfig_invalidAssetDecimals(
+        RewardsDataTypes.DistributionConfig[] memory config
+    ) private {
+        _mockDistributionConfigOrdinaryCalls(config);
+
+        uint8 invalidDecimals = 0;
+        vm.mockCall(
+            config[1].asset,
+            abi.encodeWithSelector(IERC20Metadata.decimals.selector),
+            abi.encode(invalidDecimals)
+        );
+    }
+
+    function _mockDistributionConfig_validAssetDecimals(
+        RewardsDataTypes.DistributionConfig[] memory config
+    ) private {
+        _mockDistributionConfigOrdinaryCalls(config);
+
+        uint8 validDecimals = 18;
+        vm.mockCall(
+            config[1].asset,
+            abi.encodeWithSelector(IERC20Metadata.decimals.selector),
+            abi.encode(validDecimals)
+        );
+    }
+
+    function _mockDistributionConfigOrdinaryCalls(
+        RewardsDataTypes.DistributionConfig[] memory config
+    ) private {
+        vm.mockCall(
+            solidStakingViewActions,
+            abi.encodeWithSelector(ISolidStakingViewActions.totalStaked.selector, config[0].asset),
+            abi.encode(1000)
+        );
+        vm.mockCall(
+            solidStakingViewActions,
+            abi.encodeWithSelector(ISolidStakingViewActions.totalStaked.selector, config[1].asset),
+            abi.encode(2000)
+        );
+        vm.mockCall(
+            address(config[0].rewardOracle),
+            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
+            abi.encode(15)
+        );
+        vm.mockCall(
+            address(config[1].rewardOracle),
+            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
+            abi.encode(15)
+        );
+        vm.mockCall(
+            config[0].asset,
+            abi.encodeWithSelector(IERC20Metadata.decimals.selector),
+            abi.encode(18)
+        );
+    }
+
+    function _makeInvalidOracle() internal returns (IEACAggregatorProxy rewardOracle) {
+        rewardOracle = IEACAggregatorProxy(vm.addr(5));
+
+        vm.mockCall(
+            address(rewardOracle),
+            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
+            abi.encode(0)
+        );
+    }
+
+    function _makeValidOracle() internal returns (IEACAggregatorProxy rewardOracle) {
+        rewardOracle = IEACAggregatorProxy(vm.addr(5));
+
+        vm.mockCall(
+            address(rewardOracle),
+            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
+            abi.encode(1)
+        );
+    }
+}
--- test/rewards/RewardsController.t.sol
+++ test/rewards/RewardsController.t.sol
@@ -1,168 +1,29 @@
 pragma solidity ^0.8.0;
 
-import "forge-std/Test.sol";
-import "../../contracts/interfaces/rewards/IRewardsController.sol";
-import "../../contracts/rewards/RewardsController.sol";
+import "./BaseRewardsController.sol";
 
-contract RewardsControllerTest is Test {
-    event AssetConfigUpdated(
-        address indexed asset,
-        address indexed reward,
-        uint256 oldEmission,
-        uint256 newEmission,
-        uint256 oldDistributionEnd,
-        uint256 newDistributionEnd,
-        uint256 assetIndex
-    );
-    event RewardOracleUpdated(address indexed reward, address indexed rewardOracle);
-    event ClaimerSet(address indexed user, address indexed claimer);
-    event RewardsVaultUpdated(address indexed rewardsVault);
-    event SolidStakingUpdated(address indexed solidStaking);
-
-    uint32 constant CURRENT_DATE = 1666016743;
-
-    IRewardsController rewardsController;
-    address solidStakingViewActions;
-    address rewardsVault;
-    address emissionManager;
-
-    function setUp() public {
-        vm.warp(CURRENT_DATE);
-
-        solidStakingViewActions = vm.addr(1);
-        rewardsVault = vm.addr(2);
-        emissionManager = vm.addr(3);
-
-        rewardsController = new RewardsController();
-        vm.expectEmit(true, false, false, false, address(rewardsController));
-        emit SolidStakingUpdated(solidStakingViewActions);
-        vm.expectEmit(true, false, false, false, address(rewardsController));
-        emit RewardsVaultUpdated(rewardsVault);
-        RewardsController(address(rewardsController)).setup(
-            solidStakingViewActions,
-            rewardsVault,
-            emissionManager
-        );
-
-        vm.label(address(rewardsController), "rewardsController");
-        vm.label(rewardsVault, "rewardsVault");
-        vm.label(emissionManager, "emissionManager");
-    }
-
+contract RewardsControllerTest is BaseRewardsControllerTest {
     function testConfigureAssets_failsIfNotEmissionManager() public {
-        address notEmissionManager = vm.addr(4);
-        vm.prank(notEmissionManager);
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IRewardsDistributor.NotEmissionManager.selector,
-                notEmissionManager
-            )
-        );
+        vm.prank(arbitraryAccount);
+        _expectRevert_NotEmissionManager(arbitraryAccount);
         rewardsController.configureAssets(new RewardsDataTypes.DistributionConfig[](0));
     }
 
-    function testConfigureAssets_failsForInvalidDecimals() public {
+    function testConfigureAssets_failsForInvalidAssetDecimals() public {
         RewardsDataTypes.DistributionConfig[]
-            memory config = new RewardsDataTypes.DistributionConfig[](2);
-        config[0].reward = vm.addr(4);
-        config[0].asset = vm.addr(5);
-        config[0].emissionPerSecond = 100;
-        config[0].distributionEnd = CURRENT_DATE;
-        config[0].rewardOracle = IEACAggregatorProxy(vm.addr(6));
+            memory config = _makeTestDistributionConfig_invalidAssetDecimals();
 
-        config[1].reward = vm.addr(7);
-        config[1].asset = vm.addr(8);
-        config[1].emissionPerSecond = 200;
-        config[1].distributionEnd = CURRENT_DATE;
-        config[1].rewardOracle = IEACAggregatorProxy(vm.addr(9));
-
-        vm.mockCall(
-            solidStakingViewActions,
-            abi.encodeWithSelector(ISolidStakingViewActions.totalStaked.selector, config[0].asset),
-            abi.encode(1000)
-        );
-        vm.mockCall(
-            solidStakingViewActions,
-            abi.encodeWithSelector(ISolidStakingViewActions.totalStaked.selector, config[1].asset),
-            abi.encode(2000)
-        );
-        vm.mockCall(
-            address(config[0].rewardOracle),
-            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
-            abi.encode(15)
-        );
-        vm.mockCall(
-            address(config[1].rewardOracle),
-            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
-            abi.encode(15)
-        );
-        vm.mockCall(
-            config[0].asset,
-            abi.encodeWithSelector(IERC20Metadata.decimals.selector),
-            abi.encode(18)
-        );
-        vm.mockCall(
-            config[1].asset,
-            abi.encodeWithSelector(IERC20Metadata.decimals.selector),
-            abi.encode(0)
-        );
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IRewardsDistributor.InvalidAssetDecimals.selector,
-                config[1].asset
-            )
-        );
+        _expectRevert_InvalidAssetDecimals(config[1].asset);
         vm.prank(emissionManager);
         rewardsController.configureAssets(config);
     }
 
     function testConfigureAssets() public {
         RewardsDataTypes.DistributionConfig[]
-            memory config = new RewardsDataTypes.DistributionConfig[](2);
-        config[0].reward = vm.addr(4);
-        config[0].asset = vm.addr(5);
-        config[0].emissionPerSecond = 100;
-        config[0].distributionEnd = CURRENT_DATE;
-        config[0].rewardOracle = IEACAggregatorProxy(vm.addr(6));
+            memory config = _makeTestDistributionConfig_validAssetDecimals();
 
-        config[1].reward = vm.addr(7);
-        config[1].asset = vm.addr(8);
-        config[1].emissionPerSecond = 200;
-        config[1].distributionEnd = CURRENT_DATE;
-        config[1].rewardOracle = IEACAggregatorProxy(vm.addr(9));
-
-        vm.mockCall(
-            solidStakingViewActions,
-            abi.encodeWithSelector(ISolidStakingViewActions.totalStaked.selector, config[0].asset),
-            abi.encode(1000)
-        );
-        vm.mockCall(
-            solidStakingViewActions,
-            abi.encodeWithSelector(ISolidStakingViewActions.totalStaked.selector, config[1].asset),
-            abi.encode(2000)
-        );
-        vm.mockCall(
-            address(config[0].rewardOracle),
-            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
-            abi.encode(15)
-        );
-        vm.mockCall(
-            address(config[1].rewardOracle),
-            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
-            abi.encode(15)
-        );
-        vm.mockCall(
-            config[0].asset,
-            abi.encodeWithSelector(IERC20Metadata.decimals.selector),
-            abi.encode(18)
-        );
-        vm.mockCall(
-            config[1].asset,
-            abi.encodeWithSelector(IERC20Metadata.decimals.selector),
-            abi.encode(18)
-        );
-        vm.expectEmit(true, true, false, false, address(rewardsController));
-        emit AssetConfigUpdated(config[0].asset, config[0].reward, 0, 0, 0, 0, 0);
+        _expectEmitAssetConfigUpdated(config[0].asset, config[0].reward);
+        _expectEmitAssetConfigUpdated(config[1].asset, config[1].reward);
         vm.prank(emissionManager);
         rewardsController.configureAssets(config);
 
@@ -178,137 +39,99 @@ contract RewardsControllerTest is Test {
 
     function testSetRewardOracle_failsForInvalidRewardOracle() public {
         address reward = vm.addr(4);
-        IEACAggregatorProxy rewardOracle = IEACAggregatorProxy(vm.addr(5));
+        IEACAggregatorProxy rewardOracle = _makeInvalidOracle();
 
-        vm.mockCall(
-            address(rewardOracle),
-            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
-            abi.encode(0)
-        );
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IRewardsController.InvalidRewardOracle.selector,
-                reward,
-                vm.addr(5)
-            )
-        );
+        _expectRevert_InvalidRewardOracle(reward, rewardOracle);
         vm.prank(emissionManager);
         rewardsController.setRewardOracle(reward, rewardOracle);
     }
 
     function testSetRewardOracle_failsIfNotEmissionManager() public {
-        address notEmissionManager = vm.addr(4);
-        vm.prank(notEmissionManager);
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IRewardsDistributor.NotEmissionManager.selector,
-                notEmissionManager
-            )
-        );
-        rewardsController.setRewardOracle(vm.addr(3), IEACAggregatorProxy(vm.addr(4)));
+        address reward;
+        address rewardOracle;
+
+        vm.prank(arbitraryAccount);
+        _expectRevert_NotEmissionManager(arbitraryAccount);
+        rewardsController.setRewardOracle(reward, IEACAggregatorProxy(rewardOracle));
     }
 
     function testSetRewardOracle() public {
         address reward = vm.addr(4);
-        IEACAggregatorProxy rewardOracle = IEACAggregatorProxy(vm.addr(5));
+        IEACAggregatorProxy rewardOracle = _makeValidOracle();
 
-        vm.mockCall(
-            address(rewardOracle),
-            abi.encodeWithSelector(IEACAggregatorProxy.latestAnswer.selector),
-            abi.encode(15)
-        );
-        vm.expectEmit(true, true, false, false, address(rewardsController));
-        emit RewardOracleUpdated(reward, address(rewardOracle));
         vm.prank(emissionManager);
+        _expectEmitRewardOracleUpdated(reward, rewardOracle);
         rewardsController.setRewardOracle(reward, rewardOracle);
 
         assertEq(rewardsController.getRewardOracle(reward), address(rewardOracle));
     }
 
     function testSetClaimer_failsIfNotEmissionManager() public {
-        address notEmissionManager = vm.addr(4);
-        vm.prank(notEmissionManager);
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IRewardsDistributor.NotEmissionManager.selector,
-                notEmissionManager
-            )
-        );
-        rewardsController.setClaimer(vm.addr(3), vm.addr(4));
+        address user;
+        address claimer;
+
+        vm.prank(arbitraryAccount);
+        _expectRevert_NotEmissionManager(arbitraryAccount);
+        rewardsController.setClaimer(user, claimer);
     }
 
     function testSetClaimer() public {
         address claimer = vm.addr(4);
         address user = vm.addr(5);
 
-        vm.expectEmit(true, true, false, false, address(rewardsController));
-        emit ClaimerSet(user, claimer);
         vm.prank(emissionManager);
+        _expectEmit_ClaimerSet(user, claimer);
         rewardsController.setClaimer(user, claimer);
 
         assertEq(rewardsController.getClaimer(user), claimer);
     }
 
     function testSetRewardsVault_failsIfNotEmissionManager() public {
-        address notEmissionManager = vm.addr(4);
-        vm.prank(notEmissionManager);
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IRewardsDistributor.NotEmissionManager.selector,
-                notEmissionManager
-            )
-        );
-        rewardsController.setRewardsVault(vm.addr(3));
+        vm.prank(arbitraryAccount);
+        _expectRevert_NotEmissionManager(arbitraryAccount);
+        rewardsController.setRewardsVault(rewardsVault);
     }
 
     function testSetRewardsVault() public {
         address _rewardsVault = vm.addr(4);
-        vm.expectEmit(true, false, false, false, address(rewardsController));
-        emit RewardsVaultUpdated(_rewardsVault);
+
         vm.prank(emissionManager);
+        _expectEmit_RewardsVaultUpdated(_rewardsVault);
         rewardsController.setRewardsVault(_rewardsVault);
 
         assertEq(rewardsController.getRewardsVault(), _rewardsVault);
     }
 
     function testSetSolidStaking_failsIfNotEmissionManager() public {
-        address notEmissionManager = vm.addr(4);
-        vm.prank(notEmissionManager);
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IRewardsDistributor.NotEmissionManager.selector,
-                notEmissionManager
-            )
-        );
-        rewardsController.setSolidStaking(vm.addr(3));
+        vm.prank(arbitraryAccount);
+        _expectRevert_NotEmissionManager(arbitraryAccount);
+        rewardsController.setSolidStaking(solidStakingViewActions);
     }
 
     function testSetSolidStaking() public {
-        address solidStaking = vm.addr(4);
-        vm.expectEmit(true, false, false, false, address(rewardsController));
-        emit SolidStakingUpdated(solidStaking);
+        address _solidStaking = vm.addr(4);
+
         vm.prank(emissionManager);
-        rewardsController.setSolidStaking(solidStaking);
+        _expectEmit_SolidStakingUpdated(_solidStaking);
+        rewardsController.setSolidStaking(_solidStaking);
 
         assertEq(
             address(RewardsController(address(rewardsController)).solidStakingViewActions()),
-            solidStaking
+            _solidStaking
         );
     }
 
     function testHandleUserStakeChanged_failsIfNotCalledBySolidStaking() public {
-        address notSolidStaking = vm.addr(4);
-        vm.expectRevert(
-            abi.encodeWithSelector(IRewardsController.NotSolidStaking.selector, notSolidStaking)
-        );
-        vm.prank(notSolidStaking);
+        vm.prank(arbitraryAccount);
+        _expectRevert_NotSolidStaking(arbitraryAccount);
         rewardsController.handleUserStakeChanged(vm.addr(5), vm.addr(6), 0, 0);
     }
 
     function testClaimAllRewards_failsForInvalidToAddress() public {
         address[] memory assets = new address[](0);
         address invalidTo = address(0);
-        vm.expectRevert(abi.encodeWithSelector(IRewardsDistributor.InvalidInput.selector));
+
+        _expectRevert_InvalidInput();
         rewardsController.claimAllRewards(assets, invalidTo);
     }
 
@@ -324,13 +147,13 @@ contract RewardsControllerTest is Test {
         rewardsController.setClaimer(invalidOnBehalfOf, address(this));
         vm.stopPrank();
 
-        vm.expectRevert(abi.encodeWithSelector(IRewardsDistributor.InvalidInput.selector));
+        _expectRevert_InvalidInput();
         rewardsController.claimAllRewardsOnBehalf(assets, invalidOnBehalfOf, invalidTo);
 
-        vm.expectRevert(abi.encodeWithSelector(IRewardsDistributor.InvalidInput.selector));
+        _expectRevert_InvalidInput();
         rewardsController.claimAllRewardsOnBehalf(assets, okOnBehalfOf, invalidTo);
 
-        vm.expectRevert(abi.encodeWithSelector(IRewardsDistributor.InvalidInput.selector));
+        _expectRevert_InvalidInput();
         rewardsController.claimAllRewardsOnBehalf(assets, invalidOnBehalfOf, okTo);
     }
 
@@ -338,23 +161,29 @@ contract RewardsControllerTest is Test {
         address[] memory assets = new address[](0);
         address onBehalfOf = vm.addr(4);
         address to = vm.addr(5);
-        address unauthorizedClaimer = vm.addr(6);
+
+        vm.prank(arbitraryAccount);
+        _expectRevert_UnauthorizedClaimer(arbitraryAccount, onBehalfOf);
+        rewardsController.claimAllRewardsOnBehalf(assets, onBehalfOf, to);
+    }
+
+    function testClaimAllRewardsOnBehalf() public {
+        address[] memory assets = new address[](0);
+        address onBehalfOf = vm.addr(4);
+        address to = vm.addr(5);
 
         vm.prank(emissionManager);
         rewardsController.setClaimer(onBehalfOf, address(this));
 
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                IRewardsController.UnauthorizedClaimer.selector,
-                unauthorizedClaimer,
-                onBehalfOf
-            )
-        );
-        vm.prank(unauthorizedClaimer);
         rewardsController.claimAllRewardsOnBehalf(assets, onBehalfOf, to);
+    }
 
-        rewardsController.claimAllRewardsOnBehalf(assets, onBehalfOf, to); //should not revert
+    function testClaimAllRewardsOnBehalf_solidStakingIsWhitelisted() public {
+        address[] memory assets = new address[](0);
+        address onBehalfOf = vm.addr(4);
+        address to = vm.addr(5);
+
         vm.prank(solidStakingViewActions);
-        rewardsController.claimAllRewardsOnBehalf(assets, onBehalfOf, to); //should not revert
+        rewardsController.claimAllRewardsOnBehalf(assets, onBehalfOf, to);
     }
 }
