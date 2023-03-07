1611bab9d7214c085c9f6630929850f8e37f1404
Andrei Stefan <andrei.stefan.work@gmail.com>
2023-03-07 22:22:33 +0200

    extract config to struct

--- contracts/LiquidityDeployer.sol
+++ contracts/LiquidityDeployer.sol
@@ -2,27 +2,18 @@
 pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/liquidity-deployer/ILiquidityDeployer.sol";
-import "./interfaces/liquidity-deployer/IUniProxy.sol";
 import "./libraries/liquidity-deployer/LiquidityDeployerDataTypes.sol";
 
 /// @author Solid World
 contract LiquidityDeployer is ILiquidityDeployer, ReentrancyGuard {
-    IERC20 internal immutable token0;
-    IERC20 internal immutable token1;
-    address internal immutable gammaVault;
-    IUniProxy internal immutable uniProxy;
-    uint internal immutable conversionRate;
-    uint8 internal immutable conversionRateDecimals;
+    LiquidityDeployerDataTypes.Config internal config;
+    LiquidityDeployerDataTypes.TotalDeposits internal totalDeposits;
 
     /// @dev Account => token0 balance
     mapping(address => uint) internal token0Balance;
-
     /// @dev Account => token1 balance
     mapping(address => uint) internal token1Balance;
-
-    LiquidityDeployerDataTypes.TotalDeposits internal totalDeposits;
 
     modifier validDepositAmount(uint amount) {
         if (amount == 0) {
@@ -32,19 +23,19 @@ contract LiquidityDeployer is ILiquidityDeployer, ReentrancyGuard {
     }
 
     constructor(
-        address _token0,
-        address _token1,
-        address _gammaVault,
-        address _uniProxy,
-        uint _conversionRate,
-        uint8 _conversionRateDecimals
+        address token0,
+        address token1,
+        address gammaVault,
+        address uniProxy,
+        uint conversionRate,
+        uint8 conversionRateDecimals
     ) {
-        token0 = IERC20(_token0);
-        token1 = IERC20(_token1);
-        gammaVault = _gammaVault;
-        uniProxy = IUniProxy(_uniProxy);
-        conversionRate = _conversionRate;
-        conversionRateDecimals = _conversionRateDecimals;
+        config.token0 = IERC20(token0);
+        config.token1 = IERC20(token1);
+        config.gammaVault = gammaVault;
+        config.uniProxy = IUniProxy(uniProxy);
+        config.conversionRate = conversionRate;
+        config.conversionRateDecimals = conversionRateDecimals;
     }
 
     function depositToken0(uint amount) external nonReentrant validDepositAmount(amount) {
@@ -58,31 +49,31 @@ contract LiquidityDeployer is ILiquidityDeployer, ReentrancyGuard {
     }
 
     function getToken0() external view returns (address) {
-        return address(token0);
+        return address(config.token0);
     }
 
     function getToken1() external view returns (address) {
-        return address(token1);
+        return address(config.token1);
     }
 
     /// @inheritdoc ILiquidityDeployer
     function getGammaVault() external view returns (address) {
-        return gammaVault;
+        return config.gammaVault;
     }
 
     /// @inheritdoc ILiquidityDeployer
     function getUniProxy() external view returns (address) {
-        return address(uniProxy);
+        return address(config.uniProxy);
     }
 
     /// @inheritdoc ILiquidityDeployer
     function getConversionRate() external view returns (uint) {
-        return conversionRate;
+        return config.conversionRate;
     }
 
     /// @inheritdoc ILiquidityDeployer
     function getConversionRateDecimals() external view returns (uint8) {
-        return conversionRateDecimals;
+        return config.conversionRateDecimals;
     }
 
     function token0BalanceOf(address account) external view returns (uint) {
--- contracts/libraries/liquidity-deployer/LiquidityDeployerDataTypes.sol
+++ contracts/libraries/liquidity-deployer/LiquidityDeployerDataTypes.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.16;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../../interfaces/liquidity-deployer/IUniProxy.sol";
+
 library LiquidityDeployerDataTypes {
+    struct Config {
+        IERC20 token0;
+        IERC20 token1;
+        address gammaVault;
+        IUniProxy uniProxy;
+        uint conversionRate;
+        uint8 conversionRateDecimals;
+    }
+
     struct TotalDeposits {
         uint token0Amount;
         uint token1Amount;
