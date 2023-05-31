dacd726bb7a44ababb414aaedbc95d4755b22036
Andrei Stefan <andrei.stefan.work@gmail.com>
2023-05-31 10:20:41 +0300

    * introduce zap interface * store IUniProxy, SolidStaking addresses

--- test/zap/staking/BaseSolidZapStaker.t.sol
+++ test/zap/staking/BaseSolidZapStaker.t.sol
@@ -3,20 +3,29 @@ pragma solidity 0.8.18;
 
 import "../../BaseTest_0_8_18.sol";
 import "../../../contracts/zap/staking/SolidZapStaker.sol";
+import "../../../contracts/interfaces/staking/ISolidZapStaker.sol";
 
 abstract contract BaseSolidZapStaker is BaseTest {
-    address public constant ROUTER = address(123);
+    address public ROUTER;
+    address public IUNIPROXY;
+    address public SOLIDSTAKING;
 
-    SolidZapStaker public zapStaker;
+    ISolidZapStaker public zapStaker;
 
     function setUp() public {
-        zapStaker = new SolidZapStaker(ROUTER);
+        ROUTER = vm.addr(1);
+        IUNIPROXY = vm.addr(2);
+        SOLIDSTAKING = vm.addr(3);
+
+        zapStaker = new SolidZapStaker(ROUTER, IUNIPROXY, SOLIDSTAKING);
 
         _labelAccounts();
     }
 
     function _labelAccounts() private {
         vm.label(ROUTER, "Router");
+        vm.label(IUNIPROXY, "IUniProxy");
+        vm.label(SOLIDSTAKING, "SolidStaking");
         vm.label(address(zapStaker), "ZapStaker");
     }
 }
