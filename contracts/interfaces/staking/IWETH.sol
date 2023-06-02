4f352928f79a15a3cf77e02823d538dbeb7fc2ef
Andrei Stefan <andrei.stefan.work@gmail.com>
2023-06-02 20:35:59 +0300

    SolidZapStaker approves router to spend WETH

--- contracts/interfaces/staking/IWETH.sol
+++ contracts/interfaces/staking/IWETH.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.16;
 interface IWETH {
     function deposit() external payable;
 
-    function withdraw(uint amount) external;
+    function approve(address spender, uint amount) external returns (bool);
 }
