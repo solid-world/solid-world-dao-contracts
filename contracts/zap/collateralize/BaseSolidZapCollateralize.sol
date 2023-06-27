21d0a914cfa58bc3bd7dd52c0678c9eea931d786
Andrei Stefan <andrei.stefan.work@gmail.com>
2023-06-27 20:06:31 +0300

    zap collateralize contract is erc1155 receiver

--- contracts/zap/collateralize/BaseSolidZapCollateralize.sol
+++ contracts/zap/collateralize/BaseSolidZapCollateralize.sol
@@ -2,11 +2,12 @@
 pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "../../interfaces/zap/ISolidZapCollateralize.sol";
 import "../../interfaces/staking/IWETH.sol";
 
 /// @author Solid World
-abstract contract BaseSolidZapCollateralize is ISolidZapCollateralize, ReentrancyGuard {
+abstract contract BaseSolidZapCollateralize is ISolidZapCollateralize, IERC1155Receiver, ReentrancyGuard {
     address public immutable router;
     address public immutable weth;
     address public immutable swManager;
@@ -24,5 +25,40 @@ abstract contract BaseSolidZapCollateralize is ISolidZapCollateralize, Reentranc
         forwardContractBatch = _forwardContractBatch;
 
         IWETH(weth).approve(_router, type(uint).max);
+    }
+
+    /// @dev accept transfers from swManager contract only
+    function onERC1155Received(
+        address operator,
+        address,
+        uint,
+        uint,
+        bytes memory
+    ) public virtual returns (bytes4) {
+        if (operator != swManager) {
+            return bytes4(0);
+        }
+
+        return this.onERC1155Received.selector;
+    }
+
+    /// @dev accept transfers from swManager contract only
+    function onERC1155BatchReceived(
+        address operator,
+        address,
+        uint[] memory,
+        uint[] memory,
+        bytes memory
+    ) public virtual returns (bytes4) {
+        if (operator != swManager) {
+            return bytes4(0);
+        }
+
+        return this.onERC1155BatchReceived.selector;
+    }
+
+    function supportsInterface(bytes4 interfaceId) external pure returns (bool) {
+        // ERC165 && ERC1155TokenReceiver support
+        return interfaceId == 0x01ffc9a7 || interfaceId == 0x4e2312e0;
     }
 }
