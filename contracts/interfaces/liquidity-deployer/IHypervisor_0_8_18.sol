47a5ecfcdf1bb1c29f0a0cb84039c2b840c17941
Andrei Stefan <andrei.stefan.work@gmail.com>
2023-07-04 22:50:20 +0300

    Obtain uniProxy address from hypervisor instead of storing it (#422)

--- contracts/interfaces/liquidity-deployer/IHypervisor_0_8_18.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
-
-/// @author Gamma Strategies
-interface IHypervisor {
-    function token0() external view returns (address);
-
-    function token1() external view returns (address);
-}
